2530eb1b2aa8db9532ca85c0c28c345312e0443a
IkumaOka <ikuma1018soccer06@gmail.com>
2020-08-04 12:07:52 +0900

    finish norm_flow

--- norm_flow/distribution.py
+++ norm_flow/distribution.py
@@ -1,11 +1,12 @@
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
-
-class Distribution:
+class Distribution:    
     def calc_prob(self, z):
-        p = p.zeros(z.shape[0])
+        p = np.zeros(z.shape[0])
         return p
+
 
     def plot(self, size=5):
         side = np.linspace(-size, size, 1000)
@@ -23,5 +24,55 @@ class Distribution:
 
 
 class NormalDistribution2D(Distribution):
-    def sample(self, sample_num, n_dim):
-        z = np.random.randn(sample_num, n_dim)
+    def sample(self, sample_num):
+        z = np.random.randn(sample_num, 2)
+        return z
+
+
+    def sample_tf(self, sample_num):
+        z = tf.random_normal([sample_num, 2])
+        return z
+
+
+    def calc_prob(self, z):
+        p = np.exp(-(z[:, 0]**2+z[:, 1]**2)/2)/(2*np.pi) 
+        return p
+
+
+    def calc_prob_tf(self, z):
+        p = tf.exp(-(z[:, 0]**2+z[:, 1]**2)/2)/(2*np.pi) 
+        return p
+
+
+class TargetDistribution1(Distribution):
+    def calc_prob(self, z):
+        z1, z2 = z[:, 0], z[:, 1]
+        norm = np.sqrt(z1**2+z2**2)
+        exp1 = np.exp(-0.5*((z1-2)/0.6)**2)
+        exp2 = np.exp(-0.5*((z1+2)/0.6)**2)
+        p = 0.5*((norm - 2)/0.4)**2 - np.log(exp1 + exp2)
+        return np.exp(-p)
+
+
+    def calc_prob_tf(self, z):
+        z1, z2 = z[:, 0], z[:, 1]
+        norm = tf.sqrt(z1**2+z2**2)
+        exp1 = tf.exp(-0.5*((z1-2)/0.6)**2)
+        exp2 = tf.exp(-0.5*((z1+2)/0.6)**2)
+        p = 0.5*((norm - 2)/0.4)**2 - tf.log(exp1 + exp2)
+        return tf.exp(-p)
+
+
+class TargetDistribution2(Distribution):
+    def calc_prob(self, z):
+        z1, z2 = z[:, 0], z[:, 1]
+        w1 = np.sin(0.5*np.pi*z1)
+        p = 0.5*((z2 - w1)/0.4)**2
+        return np.exp(-p)
+
+
+    def calc_prob_tf(self, z):
+        z1, z2 = z[:, 0], z[:, 1]
+        w1 = tf.sin(0.5*np.pi*z1)
+        p = 0.5*((z2 - w1)/0.4)**2
+        return tf.exp(-p)
--- norm_flow/normalizing_flow.py
+++ norm_flow/normalizing_flow.py
@@ -1,25 +1,39 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_placeholder():
+    z_0 = tf.placeholder(tf.float32, shape=[None, 2])
+    log_q_0 = tf.placeholder(tf.float32, shape=[None])
+    return z_0, log_q_0
+
+
+def calc_loss(z_k, log_q_k, target_density):
+    log_p = tf.log(target_density.calc_prob_tf(z_k)+1e-7)
+    loss = tf.reduce_mean(log_q_k - log_p, -1)
+    return loss
+
+
+def get_train(loss):
+    return tf.train.AdamOptimizer().minimize(loss)
 
 
 class PlanarFlow:
     def __init__(self, dim):
         self.dim = dim
-        self.h = lambda x: nn.Tanh(x)
-        self.h_prime = lambda x: 1 - nn.Tanh(x)**2
-        print(torch.tensor(tf.random.truncated_normal(shape=(1, self.dim))))
-        self.w = torch.tensor(tf.random.truncated_normal(shape=(1, self.dim)))
-        self.b = torch.zeros(1)
-        self.u = torch.tensor(tf.random.truncated_normal(shape=(1, self.dim)))
+        self.h = lambda x: tf.tanh(x)
+        self.h_prime = lambda x: 1 - tf.tanh(x)**2
+        self.w = tf.Variable(tf.random.truncated_normal(shape=(1, self.dim)))
+        self.b = tf.Variable(tf.zeros(shape=(1)))
+        self.u = tf.Variable(tf.random.truncated_normal(shape=(1, self.dim)))
+
 
     def __call__(self, z, log_q):
-        z += self.u * self.h((z*self.w).sum(-1).unsqueeze(-1) + self.b)
-        psi = self.h_prime((z*self.w).sum(-1).unsqueeze(-1) + self.b) * self.w
-        det_jacob = torch.abs(1 + (psi*self.u).sum(-1))
-        log_q -= torch.log(1e-7 + det_jacob)
+        z = z + self.u*self.h(tf.expand_dims(tf.reduce_sum(z*self.w, -1), -1) + self.b)
+        psi = self.h_prime(tf.expand_dims(tf.reduce_sum(z*self.w, -1), -1) + self.b)*self.w
+        det_jacob = tf.abs(1 + tf.reduce_sum(psi*self.u, -1))
+        log_q = log_q -  tf.log(1e-7 + det_jacob)
         return z, log_q
 
 
@@ -28,6 +42,7 @@ class NormalizingFlow:
         self.K = K
         self.dim = dim
         self.planar_flow = [PlanarFlow(self.dim) for i in range(self.K)]
+
 
     def __call__(self, z_0, log_q_0):
         z, log_q = self.planar_flow[0](z_0, log_q_0)
